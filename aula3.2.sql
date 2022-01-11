ec96f12f5b898b9114ab0b0bde6eaac7ba5ac34a
Flavia Nitto <flavia.nitto@accurate.com.br>
2022-01-11 10:11:29 -0300

    comando update

---
+++ aula3.2.sql
@@ -0,0 +1,20 @@
+/*Pedro e Paulo estao supervisionando o mesmo departamento*/
+INSERT INTO TB_FUNCIONARIO VALUES
+('88735410999', '1969-11-25', 'Paulo', 'F', 'Junior', 
+'Rua Havai, 88', 5000, 'M', '88735410999', 1);
+
+/*fazendo o update para mudar o dep supervisionado do Paulo*/
+UPDATE TB_FUNCIONARIO SET NUMERO_DEPARTAMENTO = 2
+WHERE CPF = '88735410999';
+
+/*quero dar 10% de aumento para todos os funcionarios do dep 1*/
+UPDATE TB_FUNCIONARIO SET SALARIO = 1.1 * SALARIO 
+WHERE NUMERO_DEPARTAMENTO = 1;
+
+/*mudando os dados cadastrais e salario do funcionario*/
+INSERT INTO TB_FUNCIONARIO VALUES
+('23455467890', '1999-11-15', 'Arthur', 'N', 'Nitto', 
+'Rua Alcacer, 220', 4000, 'M', '99087834502', 1);
+
+UPDATE TB_FUNCIONARIO SET ENDERECO = 'Av. da Saudade, 100',
+SALARIO = 4500 WHERE CPF = '23455467890';
